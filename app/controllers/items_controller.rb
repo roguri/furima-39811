f71ffdb66b4da52a31dcb2c3943ea96b9bbd51f7
roguri <ryoryoma205@gmail.com>
2024-02-23 11:23:31 +0900

    ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -7,6 +8,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+  belongs_to :region
+
+  validates :title, :price, :item_information, :image, presence: true
+  validates :category_id, :condition_id, :shipping_cost_id, :shipping_date_id, :region_id, numericality: 
+            { other_than: 1, message: "can't be blank" }
 end
