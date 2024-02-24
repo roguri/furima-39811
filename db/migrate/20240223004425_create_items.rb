112dfdce8084a57a4440b15625047f993598d3aa
roguri <ryoryoma205@gmail.com>
2024-02-24 17:53:30 +0900

    不要な記述修正

--- db/migrate/20240223004425_create_items.rb
+++ db/migrate/20240223004425_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :title
-      t.integer :price
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :shipping_cost_id
-      t.integer :shipping_date_id
-      t.integer :region_id
-      t.text    :item_information
+      t.string :title, null: false
+      t.integer :price, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :shipping_cost_id, null: false
+      t.integer :shipping_date_id, null: false
+      t.integer :region_id, null: false
+      t.text    :item_information, null: false
       t.references :user, foreign_key: true
       t.timestamps
     end
