42508118398622ccffdf8d39555f48a8aff30a82
roguri <ryoryoma205@gmail.com>
2024-02-27 19:31:10 +0900

    ユーザーログイン判断実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.order('created_at DESC')
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if user_signed_in? && current_user.id == @item.user_id
+    if current_user.id == @item.user_id
       render :edit
     else
       redirect_to root_path
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless user_signed_in? && current_user.id == @item.user_id
+    return unless current_user.id == @item.user_id
 
     @item.destroy
     redirect_to root_path
